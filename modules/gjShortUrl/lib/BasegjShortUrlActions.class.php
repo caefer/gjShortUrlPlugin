0ef20336b299fd171a69f8c25c676f0c100e5e6e
caefer <caefer@ical.ly>
2010-08-25 12:50:39 +0200

    * for routenames set as target parameters will now be added (README example 2)

--- modules/gjShortUrl/lib/BasegjShortUrlActions.class.php
+++ modules/gjShortUrl/lib/BasegjShortUrlActions.class.php
@@ -13,12 +13,19 @@ abstract class BasegjShortUrlActions extends sfActions
   public function executeRedirect(sfWebRequest $request)
   {
     $shortUrl = $this->getRoute()->getObject();
+    $target = ltrim($shortUrl->target, '@');
+    if('/' != $shortUrl->target[0])
+    {
+      $params = array_merge($request->getParameterHolder()->getAll(), array('sf_route' => $target));
+      $target = $this->getController()->genUrl($params);
+    }
+
     $this->logMessage(sprintf('{gjShortUrlPlugin} Redirected from "%s" to "%s" (%d)',
       $shortUrl->source,
-      $shortUrl->target,
+      $target,
       $shortUrl->code
     ));
 
-    $this->redirect($shortUrl->target, $shortUrl->code);
+    $this->redirect($target, $shortUrl->code);
   }
 }
