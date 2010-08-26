166b8e559951db37ccd4a39a8253e14b050dad18
caefer <caefer@ical.ly>
2010-08-26 09:04:47 +0200

    * changed bootstrap to work with PHPUnit

--- test/bootstrap/unit.php
+++ test/bootstrap/unit.php
@@ -1,15 +1,39 @@
 <?php
+/**
+ * This file is part of the gjShortUrlPlugin unit tests package.
+ * (c) 2010 Christian Schaefer <caefer@ical.ly>>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package    gjShortUrlPluginUnitTests
+ * @subpackage bootstrap
+ * @author     Christian Schaefer <caefer@ical.ly>
+ * @version    SVN: $Id: unit.php 29957 2010-06-24 08:24:23Z caefer $
+ */
 
 if (!isset($_SERVER['SYMFONY']))
 {
   throw new RuntimeException('Could not find symfony core libraries.');
 }
 
+/** symfonys autoloader */
 require_once $_SERVER['SYMFONY'].'/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
 
-$configuration = new sfProjectConfiguration(dirname(__FILE__).'/../fixtures/project');
-require_once $configuration->getSymfonyLibDir().'/vendor/lime/lime.php';
+$projectPath = dirname(__FILE__).'/../fixtures/project';
+/** configuration of the fixture project */
+require_once($projectPath.'/config/ProjectConfiguration.class.php');
+
+if(!isset($app))
+{
+  $configuration = new ProjectConfiguration($projectPath);
+}
+else
+{
+  $configuration = ProjectConfiguration::getApplicationConfiguration($app, 'test', isset($debug) ? $debug : true);
+  sfContext::createInstance($configuration);
+}
 
 function gjShortUrlPlugin_autoload_again($class)
 {
